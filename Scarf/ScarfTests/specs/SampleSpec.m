7cf864c5ee832c1f0cd92e97e148178e55eae7cf
Jabari Bell <jabari.bell@pxlflu.net>
2014-01-05 19:18:05 -0500

    moved sample spec to sample folder

---
+++ Scarf/ScarfTests/specs/SampleSpec.m
@@ -0,0 +1,26 @@
+#import "Kiwi.h"
+
+SPEC_BEGIN(MathSpec)
+
+//describe(@"Math", ^{
+//    it(@"is pretty cool", ^{
+//        NSUInteger a = 40;
+//        NSUInteger b = 3;
+//        [[theValue(a + b) should] equal:theValue(43)];
+//    });
+//});
+
+describe(@"In my first kiwi test", ^{
+    context(@"a sample string", ^{
+        NSString  *greeting = @"hello world";
+        it(@"should exist", ^{
+            [[greeting shouldNot] beNil];
+        });
+        
+        it(@"should be 'hello, world'", ^{
+            [[greeting should] equal:@"hello world"];
+        });
+    });
+});
+
+SPEC_END
